1b5021584e37c26cea6bde431a79f823b560c89d
henry <wuheng09@otcaix.iscas.ac.cn>
2018-01-09 22:12:15 +0800

    remove ununsed class

--- src/test/java/cn/cas/is/abcsys/KVToModelParametersGeneratorTest.java
+++ src/test/java/cn/cas/is/abcsys/KVToModelParametersGeneratorTest.java
@@ -12,71 +12,71 @@ import io.fabric8.kubernetes.client.DefaultKubernetesClient;
 
 /**
  * @author henry
- * @email  wuheng@otcaix.iscas.ac.cn
+ * @email wuheng@otcaix.iscas.ac.cn
  *
- * @date   2018年1月3日
+ * @date 2018年1月3日
  */
 @SuppressWarnings("serial")
 public class KVToModelParametersGeneratorTest {
 
 	/*******************************************************************************
 	 * 
-	 *                          Create Namespace
+	 * Create Namespace
 	 * 
 	 ********************************************************************************/
 	public static Map<String, Object> createNSParams = new HashMap<String, Object>();
-	
+
 	static {
 		createNSParams.put("setMetadata-setName", "wuheng");
-		createNSParams.put("setMetadata-setLabels", new HashMap<String,String>(){
+		createNSParams.put("setMetadata-setLabels", new HashMap<String, String>() {
 			{
-				put("name","busybox-wuheng");
-				put("version","20180788");
+				put("name", "busybox-wuheng");
+				put("version", "20180788");
 			}
 		});
 	}
-	
+
 	/*******************************************************************************
 	 * 
-	 *                          Delete Namespace
+	 * Delete Namespace
 	 * 
 	 ********************************************************************************/
 	public static Map<String, Object> deleteNSParams = new HashMap<String, Object>();
-	
+
 	static {
 		deleteNSParams.put("setMetadata-setName", "wuheng");
 	}
-	
+
 	/*******************************************************************************
 	 * 
-	 *                          Create Deployment
+	 * Create Deployment
 	 * 
 	 ********************************************************************************/
 	public static Map<String, Object> createDMParams = new HashMap<String, Object>();
-	
+
 	static {
 		createDMParams.put("setMetadata-setName", "busybox-dmdx");
 		createDMParams.put("setMetadata-setNamespace", "wuheng");
-		createDMParams.put("setMetadata-setLabels", new HashMap<String,String>(){
+		createDMParams.put("setMetadata-setLabels", new HashMap<String, String>() {
 			{
-				put("app","busybox-dmdx");
-				put("version","20180109");
+				put("app", "busybox-dmdx");
+				put("version", "20180109");
 			}
 		});
 		createDMParams.put("setSpec-setReplicas", 3);
 		createDMParams.put("setSpec-setTemplate-setMetadata-setName", "busybox-dmdx");
-		createDMParams.put("setSpec-setTemplate-setMetadata-setLabels", new HashMap<String,String>(){
+		createDMParams.put("setSpec-setTemplate-setMetadata-setLabels", new HashMap<String, String>() {
 			{
-				put("app","busybox-dmdx");
+				put("app", "busybox-dmdx");
 			}
 		});
 		createDMParams.put("setSpec-setTemplate-setSpec-setContainers", new ArrayList<Object>() {
 			{
-				add(new HashMap<String,Object>(){
+				add(new HashMap<String, Object>() {
 					{
-						put("setImage","dcr.io:5000/busybox:latest");
-						put("setImagePullPolicy","IfNotPresent");
-						put("setName","busybox-dmdx");
+						put("setImage", "dcr.io:5000/busybox:latest");
+						put("setImagePullPolicy", "IfNotPresent");
+						put("setName", "busybox-dmdx");
 						put("setCommand", new ArrayList<String>() {
 							{
 								add("sleep");
@@ -88,47 +88,62 @@ public class KVToModelParametersGeneratorTest {
 			}
 		});
 	}
-	
+
 	/*******************************************************************************
 	 * 
-	 *                          Delete Deployment
+	 * Delete Deployment
 	 * 
 	 ********************************************************************************/
 	public static Map<String, Object> deleteDMParams = new HashMap<String, Object>();
-	
+
 	static {
 		deleteDMParams.put("setMetadata-setName", "busybox-dmdx");
 		deleteDMParams.put("setMetadata-setNamespace", "wuheng");
 	}
-	
+
 	/*******************************************************************************
 	 * 
-	 *                          Update Deployment
+	 * Update Deployment
 	 * 
 	 ********************************************************************************/
 	public static Map<String, Object> updateDMParams = new HashMap<String, Object>();
-	
+
 	static {
 		updateDMParams.put("setMetadata-setName", "busybox-dmdx");
 		updateDMParams.put("setMetadata-setNamespace", "wuheng");
 		updateDMParams.put("setSpec-setReplicas", 1);
 	}
-	
+
 	/**
 	 * @param args
-	 * @throws Exception 
+	 * @throws Exception
 	 */
 	@SuppressWarnings({ "resource", "unused" })
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
 		DefaultKubernetesClient client = new DefaultKubernetesClient("http://118.190.46.58:9888");
 		KubernetesModelParametersGenerator generator = new KubernetesModelParametersGenerator();
-//		System.out.println(generator.delete(client, "Namespace", deleteNSParams));
-//		System.out.println(generator.create(client, "Namespace", createNSParams));
-//		System.out.println(generator.create(client, "Deployment", createDMParams));
-//		System.out.println(generator.delete(client, "Deployment", deleteDMParams));
+		// System.out.println(generator.delete(client, "Namespace", deleteNSParams));
+		// System.out.println(generator.create(client, "Namespace", createNSParams));
+		// System.out.println(generator.create(client, "Deployment", createDMParams));
+		// System.out.println(generator.delete(client, "Deployment", deleteDMParams));
 		System.out.println(generator.scaleTo(client, "Deployment", "wuheng", "busybox-dmdx", 8));
-		
+
+		// DefaultKubernetesClient client = new
+		// DefaultKubernetesClient("http://127.0.0.1:9888");
+		//
+		// MixedOperation<Deployment, DeploymentList, DoneableDeployment,
+		// ScalableResource<Deployment, DoneableDeployment>> deployments =
+		// client.extensions().deployments();
+		// Deployment deployment = new Deployment();
+		//
+		// // create
+		// deployments.create(deployment );
+		// // update
+		// deployments.inNamespace("").withName("").scale(2);
+		// // delete
+		// deployments.delete(deployment);
+
 	}
 
 }
