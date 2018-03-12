41304b87fd294ebca9d7f4028b30199025689791
henry <wuheng09@otcaix.iscas.ac.cn>
2018-03-12 21:36:12 +0800

    support env

--- jointware-services/jointware-containers/src/test/java/com/github/isdream/jointware/containers/handlers/KubernetesHandler.java
+++ jointware-services/jointware-containers/src/test/java/com/github/isdream/jointware/containers/handlers/KubernetesHandler.java
@@ -22,9 +22,19 @@ public class KubernetesHandler extends AbstractHandler {
 	
 	public final static String KEY_RESOURCES = "resources";
 	
-	public final static String REQUEST_RESOURCES = "requestsResources";
+	public final static String KEY_REQUEST_RESOURCES = "requestsResources";
 	
-	public final static String LIMITES_RESOURCES = "limitsResources";
+	public final static String KEY_LIMITES_RESOURCES = "limitsResources";
+	
+	public final static String TYPE_ENV = "env";
+	
+	public final static String TYPE_PORT = "port";
+	
+	public final static String TYPE_PROBE = "probe";
+	
+	public final static String TYPE_RESOURCE = "resource";
+	
+	public final static String TYPE_VOLUMEMOUNT = "volumeMount";
 	
 	protected final Map<String, String> valuesConvertor = new HashMap<String, String>();
 	
@@ -33,6 +43,8 @@ public class KubernetesHandler extends AbstractHandler {
 	protected Map<String, String> typesConvertor;
 	
 	protected Map<String, Map<String, Object>> originRequest;
+	
+	protected Map<String, Map<String, Object>> newRequests = new HashMap<String, Map<String, Object>>();
 	
 	protected int n = 0;
 	
@@ -57,40 +69,82 @@ public class KubernetesHandler extends AbstractHandler {
 	protected Map<String, Map<String, Object>> doConvert(
 				String originType, String newType) {
 		
-		Map<String, Map<String, Object>> newRequests = new HashMap<String, Map<String, Object>>();
-		Map<String, Object> oldMap = originRequest.remove(originType);
 		Map<String, Object> newMap = getMap(newRequests, newType);
 		
-		for (String key : oldMap.keySet()) {
+		try {
+			Map<String, Object> oldMap = originRequest.get(originType);
 			
-			Object value = oldMap.get(key);
-			
-			if (value.getClass().getName().equals(AbstractHandler.IS_LIST)) {
-				List<String> list = (List<String>) value;
+			for (String key : oldMap.keySet()) {
 				
-				System.out.println(key);
+				Object value = oldMap.get(key);
 				
-				if (list == null || list.size() == 0) {
-					continue;
-				}
-				
-				if(list.get(0).startsWith(tag)) {
-					List<String> newValues = new ArrayList<String>();
-					for (String thisValue : list) {
-						newValues.add(ModelParameterGenerator.JOINTWARE + n++ + "-" + getValue(typesConvertor, thisValue));
-						valuesConvertor.put(thisValue, ModelParameterGenerator.JOINTWARE + "-" + getValue(typesConvertor, thisValue));
-						newRequests.putAll(this.doConvert(originType, newType));
+				if (value.getClass().getName().equals(AbstractHandler.IS_LIST)) {
+					List<String> list = (List<String>) value;
+					
+					if (list == null || list.size() == 0) {
+						continue;
 					}
-					newMap.put(keysConvertor.get(originType).get(key), newValues);
+					
+					if(list.get(0).startsWith(tag)) {
+						
+						List<String> newValues = new ArrayList<String>();
+						for (String thisValue : list) {
+							
+							if (TYPE_ENV.equals(getRealType(thisValue))) {
+								List<Map<String, String>> thisMap = (List<Map<String, String>>) originRequest.get(thisValue);
+								
+								for (int i = 0; i < thisMap.size(); i++) {
+									newValues.add(ModelParameterGenerator.JOINTWARE + ++n + "-" + getValue(typesConvertor, thisValue));
+									Map<String, Object> tempMap = getMap(newRequests, ModelParameterGenerator.JOINTWARE + n + "-" + getValue(typesConvertor, thisValue));
+									for (String thisKey : thisMap.get(i).keySet()) {
+										tempMap.put(keysConvertor.get(TYPE_ENV).get(thisKey)
+												, thisMap.get(i).get(thisKey));
+									}
+								}
+								
+								newRequests.get(valuesConvertor.get(originType)).put(keysConvertor.get(getRealType(originType)).get(key), newValues);
+								continue;
+							} else if (TYPE_RESOURCE.equals(getRealType(thisValue))) {
+								System.out.println("This is resource");
+								continue;
+							}
+							
+							newValues.add(ModelParameterGenerator.JOINTWARE + ++n + "-" + getValue(typesConvertor, thisValue));
+							valuesConvertor.put(thisValue, ModelParameterGenerator.JOINTWARE + n + "-" + getValue(typesConvertor, thisValue));
+							doConvert(
+									thisValue, ModelParameterGenerator.JOINTWARE + n + "-" 
+										+ getValue(typesConvertor, thisValue));
+						}
+						newMap.put(keysConvertor.get(getRealType(originType)).get(key), newValues);
+					} else {
+						newMap.put(keysConvertor.get(getRealType(originType)).get(key), oldMap.get(key));
+					}
+					
 				} else {
-					newMap.put(keysConvertor.get(originType).get(key), oldMap.get(key));
+					newMap.put(keysConvertor.get(getRealType(originType)).get(key), oldMap.get(key));
 				}
-				
-			} else {
-				newMap.put(keysConvertor.get(originType).get(key), oldMap.get(key));
 			}
+		} catch (Exception e) {
+			
+			if (TYPE_PORT.equals(getRealType(originType))) {
+				List<Map<String, String>> oldMap = (List<Map<String, String>>) originRequest.get(originType);
+			} else if (TYPE_VOLUMEMOUNT.equals(getRealType(originType))) {
+				List<Map<String, String>> oldMap = (List<Map<String, String>>) originRequest.get(originType);
+			} else if (TYPE_PROBE.equals(getRealType(originType))) {
+				Map<String, Object> oldMap = originRequest.get(originType);
+			} else if (TYPE_RESOURCE.equals(getRealType(originType))) {
+				Map<String, Object> oldMap = originRequest.get(originType);
+			} else {
+				throw new UnsupportedOperationException();
+			}
+			
 		}
 		return newRequests;
+	}
+	
+	protected String getRealType(String type) {
+		int idx = type.indexOf("-");
+		return (idx == -1) ? DEFAULT_TYPE : type.substring(idx + 1);
 	}
 
 	protected void checkValid(Map<String, Map<String, Object>> originRequest, String tag, String kind) {
